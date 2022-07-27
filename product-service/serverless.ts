610d2fdb474a8138ed7633d5050b810ca1815833
Dzmitry_Melnikau1 <dzmitry_melnikau1@epam.com>
2022-07-27 08:31:15 +0200

    Task 4 solution for be

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -10,17 +10,17 @@ const serverlessConfiguration: AWS = {
   ],
   provider: {
     name: 'aws',
-    runtime: 'nodejs16.x',
+    runtime: 'nodejs14.x',
     stage: 'dev',
     region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
-    /*environment: {
+    environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-    },*/
+    },
   },
   useDotenv: true,
   // import the function via paths
@@ -74,7 +74,7 @@ const serverlessConfiguration: AWS = {
       minify: false,
       sourcemap: true,
       exclude: ['aws-sdk'],
-      target: 'node16',
+      target: 'node14',
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
