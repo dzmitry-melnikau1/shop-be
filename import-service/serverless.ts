b694047c0273a991c748ff4a93f29525e5e08d23
Dzmitry_Melnikau1 <dzmitry_melnikau1@epam.com>
2022-07-31 23:45:39 +0200

    Task-5: Integration with S3 - solution for be

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,7 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
-  service: 'product-service',
+  service: 'import-service',
   frameworkVersion: '3',
   variablesResolutionMode: '20210219',
   plugins: [
@@ -11,7 +11,7 @@ const serverlessConfiguration: AWS = {
   ],
   provider: {
     name: 'aws',
-    runtime: 'nodejs12.x',
+    runtime: 'nodejs16.x',
     stage: 'dev',
     region: 'eu-west-1',
     apiGateway: {
@@ -22,51 +22,55 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: ' s3:*',
+        Resource: `arn:aws:s3:::${process.env.PRODUCT_CATALOGUE_S3_BUCKET}`
+      },
+    ],
   },
   useDotenv: true,
   // import the function via paths
   functions: {
-    getProduct: {
-      handler: 'handler.getProductById',
+    importProductsFile: {
+      handler: 'handler.importProductsFile',
       events: [
         {
           http: {
             method: 'get',
-            path: '/products/{productId}',
+            path: '/import',
             cors: true,
-          } as any
-        }
-      ]
-    },
-    getProducts: {
-      handler: 'handler.getAllProducts',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: '/products',
-            cors: true,
-          } as any
-        }
-      ]
-    },
-    createProduct: {
-      handler: 'handler.createProduct',
-      events: [
-        {
-          http: {
-            method: 'post',
-            path: '/products',
-            cors: true,
-            /*request: {
-              schema: {
-                'application/json': 'src/schemas/createProductSchema.json'
+            request: {
+              parameters: {
+                querystring: {
+                  name: true
+                }
               }
-            }*/
+            }
           } as any
         }
       ]
     },
+    importFileParser: {
+      handler: 'handler.importFileParser',
+      events: [
+        {
+          s3: {
+            bucket: process.env.PRODUCT_CATALOGUE_S3_BUCKET,
+            event: 's3:ObjectCreated:*',
+            rules: [
+              {
+                prefix: process.env.UPLOADED_PATH,
+                suffix: '.csv'
+              }
+            ],
+            existing: true
+          }
+        }
+
+      ]
+    }
   },
   package: { individually: true },
   custom: {
